eaf61c293c1b74bc08eaef27387e1e765085c950
Nick <nickyli13@outlook.com>
2025-01-01 17:08:37 -0500

    compute results inside domain

--- Compute.cpp
+++ Compute.cpp
@@ -136,15 +136,13 @@ void ComputeArea() {
 //function discretizes the boundary into nseg, 
 void FlowAroundCylinder() {
 
-	std::cout << "Heat flow past cylinder using the direct BE method." << std::endl;
-
 	double q, k, radius, radiusOuter, deltaTheta;
 	int numseg;
 	//kernel matrices
 	std::vector<std::vector<double>> deltaT;
 	std::vector<std::vector<double>> deltaU;
 	//vector with segment center potential
-	std::vector<double> u;
+	Eigen::VectorXd u;
 	//vector for deltaU times t0 (applied flow)
 	std::vector<double> F;
 	//applied flow i.e boundary condition
@@ -154,16 +152,26 @@ void FlowAroundCylinder() {
 	//each column correspond with a point
 	std::vector<std::vector<double>> coordA, coordB;
 	//coordinate of the center point Pi
+	//used for solution and postprocessing
 	std::vector<std::vector<double>> coordPi;
 	//temp tangent vector for each segment
-	std::vector<double> Vt(2);
+	std::vector<std::vector<double>> Vt;
 	//temp normal unit vector for each segment
-	std::vector<double> Vn(2);
+	std::vector<std::vector<double>> Vn;
 	//temp vectors pointing from Pi to A and B of segment
+	//also used for postprocessing
 	std::vector<double> vrA(2);
 	std::vector<double> vrB(2);
 	//segment length
 	double length;
+	//number of domain points to compute temp and flow
+	int inPoints = 0;
+	//coordinates of interior points, first row x second row y
+	std::vector<std::vector<double>> coordPa;
+	//vector containing domain point temperatures
+	std::vector<double> uP;
+	//vector containing domain point flows, first row x second row y
+	std::vector<std::vector<double>> qP;
 
 	//read input file
 	std::string junk;
@@ -175,13 +183,16 @@ void FlowAroundCylinder() {
 	}
 
 	infile >> junk >> q >> junk >> k >> junk >> radius >> junk >> numseg;
-	infile.close();
+	infile >> junk;
+	if (junk == "inpoint") {
+		infile >> junk >> inPoints;
+		coordPa.resize(2, std::vector<double>(inPoints));
+		for (size_t i = 0; i < inPoints; i++) {
+			infile >> junk >> coordPa[0][i] >> coordPa[1][i];
+		}
+	}
 
-	//print inputs to console
-	std::cout << "Heat flow input: " << q << std::endl;
-	std::cout << "Thermal conductivity: " << k << std::endl;
-	std::cout << "Cylinder radius: " << radius << std::endl;
-	std::cout << "Number of segments: " << numseg << std::endl;
+	infile.close();
 
 	deltaT.resize(numseg, std::vector<double>(numseg));
 	deltaU.resize(numseg, std::vector<double>(numseg));
@@ -190,6 +201,9 @@ void FlowAroundCylinder() {
 	t0.resize(numseg);
 	F.resize(numseg);
 	coordPi.resize(2, std::vector<double>(numseg));
+	//different form than coord vectors because need to use DotProduct function
+	Vt.resize(numseg, std::vector<double>(2));
+	Vn.resize(numseg, std::vector<double>(2));
 
 	/* FILL THE COORDINATE VECTORS WITH THE SEGMENT START AND END POINTS */
 
@@ -229,6 +243,7 @@ void FlowAroundCylinder() {
 	}
 
 	/* ASSEMBLE THE DELTA_T AND DELTA_U MATRICES */
+
 	//two loops. outer loop loops over each "source" segment Q
 	//inner loop loops over each segment again, treating as the "potential" point Pi. loop over each segment and find the contribution of the source segment Q to each
 	double c = 0.5 / PI;
@@ -239,9 +254,9 @@ void FlowAroundCylinder() {
 		double dy = coordA[1][i] - coordB[1][i];
 		length = sqrt(dx * dx + dy * dy);
 		//tangent vector
-		Vt = { (dx / length), (dy / length) };
+		Vt[i] = {(dx / length), (dy / length)};
 		//normal vector
-		Vn = { Vt[1], (- 1.0 * Vt[0])};
+		Vn[i] = {Vt[i][1], (-1.0 * Vt[i][0])};
 		for (size_t j = 0; j < numseg; j++) {
 			//compute distance from Pi to start and end of segment rA and rB
 			vrA[0] = coordA[0][i] - coordPi[0][j];
@@ -252,10 +267,10 @@ void FlowAroundCylinder() {
 			double rB = sqrt(vrB[0] * vrB[0] + vrB[1] * vrB[1]);
 			//calculate the cosines and sines of the angles first, and then backcalculate the actual angle thetaA and thetaB
 			//angles are in terms of local coordinates defined by the segment i. use the normal and tangent vectors to define the local x and y
-			double cosThetaA = DotProduct(Vn, vrA) / rA;
-			double cosThetaB = DotProduct(Vn, vrB) / rB;
-			double sinThetaA = DotProduct(Vt, vrA) / rA;
-			double sinThetaB = DotProduct(Vt, vrB) / rB;
+			double cosThetaA = DotProduct(Vn[i], vrA) / rA;
+			double cosThetaB = DotProduct(Vn[i], vrB) / rB;
+			double sinThetaA = DotProduct(Vt[i], vrA) / rA;
+			double sinThetaB = DotProduct(Vt[i], vrB) / rB;
 			//compute angles
 			double thetaA = acos(cosThetaA) * (sinThetaA >= 0 ? 1.0 : -1.0);
 			double thetaB = acos(cosThetaB) * (sinThetaB >= 0 ? 1.0 : -1.0);
@@ -289,16 +304,94 @@ void FlowAroundCylinder() {
 		}
 	}
 
-	// Regularization, since the system is very ill-conditioned 
-	double lambda = 1e-12;  // Small regularization parameter
+	// Regularization, since the system is sensitive to input precision
+	double lambda = 1e-6;  // Small regularization parameter
 	Eigen::MatrixXd deltaT_regularized = deltaT_eigen + lambda * Eigen::MatrixXd::Identity(deltaT_eigen.rows(), deltaT_eigen.cols());
 
 	// Solve with regularization
-	Eigen::VectorXd u_eigen = deltaT_regularized.colPivHouseholderQr().solve(F_eigen);
+	u = deltaT_regularized.colPivHouseholderQr().solve(F_eigen);
+
+	/* COMPUTE RESULTS IN DOMAIN (OUTSIDE OF CYLINDER) */
+
+	uP.resize(inPoints, 0.0);
+	qP.resize(2, std::vector<double>(inPoints, 0.0));
+
+	if (inPoints > 0) {
+		for (size_t i = 0; i < inPoints; i++) {
+			//for each domain point, need to loop over each segment of boundary to get contribution from each segment to flow and potential 
+			for (size_t j = 0; j < numseg; j++) {
+				//need angles between the boundary point and domain point
+				//calculated same way as previous but the sign on the normal vector is times -1 since normal vec points inwards, and domain is outside of cylinder
+				vrA[0] = coordA[0][j] - coordPa[0][i];
+				vrA[1] = coordA[1][j] - coordPa[1][i];
+				vrB[0] = coordB[0][j] - coordPa[0][i];
+				vrB[1] = coordB[1][j] - coordPa[1][i];
+				double rA = sqrt(vrA[0] * vrA[0] + vrA[1] * vrA[1]);
+				double rB = sqrt(vrB[0] * vrB[0] + vrB[1] * vrB[1]);
+
+				double cosThetaA = -1 * DotProduct(Vn[j], vrA) / rA;
+				double cosThetaB = -1 * DotProduct(Vn[j], vrB) / rB;
+				double sinThetaA = -1 * DotProduct(Vt[j], vrA) / rA;
+				double sinThetaB = -1 * DotProduct(Vt[j], vrB) / rB;
+				double h = rA * cosThetaA;
+				//compute angles
+				double thetaA = acos(cosThetaA) * (sinThetaA >= 0 ? 1.0 : -1.0);
+				double thetaB = acos(cosThetaB) * (sinThetaB >= 0 ? 1.0 : -1.0);
+				//adjust thetaA to have thetaB-thetaA lower than 180deg
+				if ((thetaB - thetaA) > PI) {
+					thetaA += 2.0 * PI;
+				}
+
+				//compute the domain point temperature by adding contributions from each segment
+				//use deltaT, deltaU, and segment temp and tractions to find potential. definition of dT and dU same as before
+				double dT = c * (thetaB - thetaA);
+				double dU = c1 * ((rB * sinThetaB * (log(rB) - 1) + thetaB * rB * cosThetaB) - (rA * sinThetaA * (log(rA) - 1) + thetaA * rA * cosThetaA));
+				uP[i] += dU * t0[j] - dT * u[j];
+
+				//compute the domain point flows in x and y direction
+				double dSx = c1 * (thetaB - thetaA);
+				double dSy = 0.0;
+				double frac = cosThetaB / cosThetaA;
+				if (frac > 0.0) {
+					dSy = -1 * c1 * log(frac);
+				}
+				double dRx = -1 * c / h * (cosThetaB * sinThetaB - cosThetaA * sinThetaA);
+				double dRy = c / h * (cosThetaB * cosThetaB - cosThetaA * cosThetaA);
+				//compute the flows in terms of local coordinates first
+				double qPx = -1 * k * (dSx * t0[j] - dRx * u[j]);
+				double qPy = -1 * k * (dSy * t0[j] - dRy * u[j]);
+				//convert to global coordinates and add the contributions
+				qP[0][i] += qPx * Vn[j][0] - qPy * Vn[j][1];
+				qP[1][i] += qPx * Vn[j][1] + qPy * Vn[j][0];
+			}
+			//make some sort of adjustment idek why. flow in y need to add the applied flow to the domain from the input file
+			uP[i] -= q / k * coordPa[1][i];
+			qP[1][i] += q;
+		}
+	}
+
+	/* PRINT RESULTS */
+
+	std::cout << "Heat flow past cylinder using the direct BE method." << std::endl;
+
+	//print inputs to console
+	std::cout << "Heat flow input: " << q << std::endl;
+	std::cout << "Thermal conductivity: " << k << std::endl;
+	std::cout << "Cylinder radius: " << radius << std::endl;
+	std::cout << "Number of segments: " << numseg << std::endl;
 
 	//print boundary results to console
-	std::cout << "Regularization: " << std::endl;
+	std::cout << "Temperature at segment centers: " << std::endl;
 	for (size_t i = 0; i < numseg; i++) {
-		std::cout << "Segment " << i + 1 << ": " << u_eigen[i] - (q / k * coordPi[1][i]) << std::endl;
+		std::cout << "Segment " << i + 1 << ": " << u[i] - (q / k * coordPi[1][i]) << std::endl;
+	}
+
+	//print domain results to console
+	std::cout << "Temperature and flow at domain points: " << std::endl;
+	for (size_t i = 0; i < inPoints; i++) {
+		std::cout << "Coordinates: x=" << coordPa[0][i] << " y=" << coordPa[1][i] << "   ";
+		std::cout << "T=" << uP[i] << "   ";
+		std::cout << "q-x=" << qP[0][i] << "   q-y=" << qP[1][i];
+		std::cout << std::endl;
 	}
 }
